c04faa5c3f85af7f478c8cd3cd1eaa7583032c4a
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2025-03-11 22:48:50 +0300

    tailwind güncellemesi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 // src/pages/_app.js
 import "../styles/globals.css";
-import Layout from "../components /Layout";
+import Layout from "../components/Layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
     </Layout>
   );
 }
