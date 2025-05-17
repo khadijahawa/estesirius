9fe30c28b3bdb6bb9fc69907881800d7ce61108f
khadija <khadijahawa555@gmail.com>
2025-05-17 16:09:11 +0300

    added snap thing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // src/pages/_app.js
 import "../styles/globals.css";
 import Layout from "../components/Layout";
@@ -32,6 +33,28 @@ export default function MyApp({ Component, pageProps }) {
         <title>Estesirius</title>
         <html lang={locale} dir={getDirection(locale)} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Snap Pixel Code */}
+        <script type="text/javascript">
+          {(function (e, t, n) {
+            if (e.snaptr) return;
+            var a = (e.snaptr = function () {
+              a.handleRequest
+                ? a.handleRequest.apply(a, arguments)
+                : a.queue.push(arguments);
+            });
+            a.queue = [];
+            var s = "script";
+            var r = t.createElement(s);
+            r.async = !0;
+            r.src = n;
+            var u = t.getElementsByTagName(s)[0];
+            u.parentNode.insertBefore(r, u);
+          })(window, document, "https://sc-static.net/scevent.min.js")}
+          snaptr('init', '6f5ff748-d361-453c-b897-561b08028264', {});
+          snaptr('track', 'PAGE_VIEW');
+        </script>
+        {/* End Snap Pixel Code */}
       </Head>
       <IconManager iconUrl={faviconUrl} />
       <IntlProvider locale={locale} messages={messages[locale]}>
