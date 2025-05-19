a6464162112afc4869261c6d673e0dbd16b3db3a
khadija <khadijahawa555@gmail.com>
2025-05-19 16:08:43 +0300

    added the google tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,31 +29,6 @@ export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const { locale } = useRouter();
 
-  useEffect(() => {
-    // Only run this on the client-side (in the browser)
-    if (typeof window !== "undefined") {
-      // Snap Pixel Code
-      (function (e, t, n) {
-        if (e.snaptr) return;
-        var a = (e.snaptr = function () {
-          a.handleRequest
-            ? a.handleRequest.apply(a, arguments)
-            : a.queue.push(arguments);
-        });
-        a.queue = [];
-        var s = "script";
-        var r = t.createElement(s);
-        r.async = !0;
-        r.src = n;
-        var u = t.getElementsByTagName(s)[0];
-        u.parentNode.insertBefore(r, u);
-      })(window, document, "https://sc-static.net/scevent.min.js");
-
-      snaptr("init", "6f5ff748-d361-453c-b897-561b08028264", {});
-      snaptr("track", "PAGE_VIEW");
-    }
-  }, []);
-
   return (
     <>
       <Head>
